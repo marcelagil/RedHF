85d399614e9157dc18429de09ff8d15a8ef866c9
marcelagil <marcela.gil@usach.cl>
2021-01-31 20:00:00 -0500

    Primer commit

---
+++ test/RedHFUnit.js
@@ -0,0 +1,27 @@
+/* *********************************************************************************************************************
+*                                                                                                                                                                                                                        *        PROYECTO:     Comparación de un método de encriptación tradicional, y Blockchain en un entorno IoT                                                                                             *
+*           ARCHIVO:     RedHFUnit.cto                                                                                   *                                                                                                                     
+*                                                                                                                      
+*    DESCRIPCION:     Define los diferentes recursos de la red HF                                                                                                                                               ---                                                                                              *
+*   REQUERIMIENTOS:    Red de negocio implementada                                    ---                                                                                              *
+*         AUTOR:     MARCELA GIL SANHUEZA ---                     
+*        * VERSION:                                                                                     *
+*        CREADO:     31/01/2021                                                                                         *
+*       REVISION:                                                                                             *
+*             
+* *********************************************************************************************************************/
+
+'use strict';
+
+// Constants
+const ADMIN_CONNECTION = require('composer-admin').AdminConnection;
+const BUSINESS_NETWORK_CONNECTION = require('composer-client').BusinessNetworkConnection;
+
+require('chai').should();
+
+describe('xx', () => {
+
+    before(() => {
+
+    });
+});
